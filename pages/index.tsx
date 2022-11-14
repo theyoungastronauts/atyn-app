3a84a421f919269b01e26274b7d2ea76b711ea85
Tyler Savery <tyler@theyoungastronauts.com>
2022-11-14 18:43:02 -0500

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,42 @@ import Head from 'next/head'
 import Image from 'next/image'
 import AppContainer from '../components/AppContainer'
 
-
+import { NextSeo } from 'next-seo';
 
 const Home: NextPage = (props) => {
 
+  const title = "A Theater Near You";
+  const description = "The mischievous staff at Sprocket Cinema finds more entertainment in tormenting each other than in the films they screen. Heists, initiations, awkward dates, and unconventional customer interactions all lead up to a midnight film premier – the closest to Hollywood these friends will ever be.";
+  const url = "https://atny.movie/";
+
   return (
-    <AppContainer />
+    <>
+      <NextSeo
+        title={title}
+        description={description}
+        canonical={url}
+        openGraph={{
+          url: url,
+          title: title,
+          description: description,
+          images: [
+            {
+              url: 'https://atny.movie/og.jpg',
+              width: 1200,
+              height: 627,
+              alt: title,
+              type: 'image/jpeg',
+            },
+
+          ],
+          site_name: title,
+        }}
+        twitter={{
+          cardType: 'summary_large_image',
+        }}
+      />
+      <AppContainer />
+    </>
   )
 }
 
